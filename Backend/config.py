70c4a2c3c34cfe9ee6a68d10c22d295bbfb3ebf2
Tom_cielo <tom_cielo@hotmail.com>
2024-01-20 13:46:55 +0100

    added database connector class

---
+++ Backend/config.py
@@ -0,0 +1,27 @@
+"""
+Connects to a SQL database using pyodbc
+""" 
+import pyodbc
+
+
+class DatabaseConnector:
+    def __init__(self):
+        self.SERVER = 'groep6server.database.windows.net'
+        self.DATABASE = 'PlantenZiektenHerkenner'
+        self.USERNAME = 'tom_cielo@groep6server'
+        self.PASSWORD = 'Scoerabi294979!'
+        self.DRIVER= '{ODBC Driver 18 for SQL Server}'
+        self.conn = None
+
+    def connect(self):
+        if not self.conn:
+            try:
+                self.connect = pyodbc.connect(f'DRIVER={self.DRIVER};SERVER={self.SERVER};'
+                                           f'DATABASE={self.DATABASE};UID={self.USERNAME};PWD={self.PASSWORD}')
+            except Exception as e:
+                print(f'Error: {e}')
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            print("Connection closed")
