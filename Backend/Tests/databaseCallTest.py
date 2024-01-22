7d25d34532b74d066439636e07f1279b8ef4517e
Tom_cielo <tom_cielo@hotmail.com>
2024-01-22 19:49:04 +0100

    working workflow

--- Backend/Tests/databaseCallTest.py
+++ Backend/Tests/databaseCallTest.py
@@ -1,22 +1,6 @@
-
-# import sys
-# import os
 from Backend.databaseConnector import DatabaseConnector
 from Backend.Models.checksModel import ChecksModel
 from Backend.DAO.checkDao import ChecksDao
-# from Models import checksModel
-# from DAO import checkDao
-# # Get the current script's directory
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-# # Add the directories containing your modules to sys.path
-# sys.path.insert(0, os.path.join(current_dir, 'Backend', 'Models'))
-# sys.path.insert(0, os.path.join(current_dir, 'Backend', 'DAO'))
-
-# print(f"Path: {sys.path}")
-
-
-# from checksModel import ChecksModel
-# from checkDao import ChecksDao
 
 db = DatabaseConnector()
 db.connect()
--- Backend/databaseConnector_example.py
+++ Backend/databaseConnector_example.py
@@ -10,5 +10,31 @@ DRIVER= '{ODBC Driver 18 for SQL Server}'
 
 connectionString = f'DRIVER={DRIVER};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
 
-conn = pyodbc.connect(connectionString)
-print(conn)
+def connect(self):
+            if not self.conn:
+                try:
+                    self.conn = pyodbc.connect(f'DRIVER={self.DRIVER};SERVER={self.SERVER};'
+                                            f'DATABASE={self.DATABASE};UID={self.USERNAME};PWD={self.PASSWORD}')
+                    print("Connected to the database")
+                except Exception as e:
+                    print(f'Error connecting to the database: {e}')
+
+def close(self):
+    if self.conn:
+        self.conn.close()
+        print("Connection closed")
+    
+def executeQuery(self, query, params=None):
+    if not self.conn:
+        print("Not connected to the db")
+        return;
+
+    with self.conn.cursor() as cursor:
+        try:
+            cursor.execute(query, params) if params else cursor.execute(query)
+            self.conn.commit()
+            print("Query executed successfully")
+        except Exception as e:
+            print(f"Error executing the query: {e}")
+            self.conn.rollback()
+        
